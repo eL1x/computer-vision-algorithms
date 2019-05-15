afcc11b9e91c49f85706ff9153b08c58041fc44a
eL1x <ragerrozz@gmail.com>
2019-05-15 03:37:21 +0200

    Attempt to add feed from camera (don't work actually)

--- optical-flow/lucas-kanade.py
+++ optical-flow/lucas-kanade.py
@@ -4,35 +4,37 @@ import matplotlib.pyplot as plt
 
 
 def lucas_kanade(image1, image2):
+    # Init image flow
     u = np.zeros(image1.size)
     v = np.zeros(image1.size)
 
     Ix, Iy, It = compute_derivatives(image1, image2)
 
+    # Compute elements needed to build A.T*A
     Ix2 = Ix**2
     Iy2 = Iy**2
     IxIy = Ix * Iy
     IxIt = Ix * It
     IyIt = Iy * It
 
-    window_size = 3
+    # Compute sum of elements in window
+    window_size = 17
     Ix2_sum = compute_window_sum(Ix2, window_size)
     Iy2_sum = compute_window_sum(Iy2, window_size)
     IxIy_sum = compute_window_sum(IxIy, window_size)
     IxIt_sum = compute_window_sum(IxIt, window_size)
     IyIt_sum = compute_window_sum(IyIt, window_size)
 
-    detA = Ix2_sum * Iy2_sum - IxIy_sum**2
-    detA[np.where(detA == 0)] = 10**(-5) # To prevent division by zero
-    print(detA.min())
-    # print(Ix2 * Iy2)
-    # print('\n\n\n')
-    # print(IxIy**2)
-    u = IxIy_sum*IyIt_sum - Iy2_sum*IxIt_sum
-    v = IxIt_sum*IxIy_sum - Ix2_sum*IyIt_sum
-    u = u/detA
-    v = v/detA
+    # Solve equation v = (A.T*A)^(-1) * A.T*b
+    det = Ix2_sum * Iy2_sum - IxIy_sum**2
+    det[np.where(det == 0)] = 1 # To prevent division by zero
+    # print(det.min())
+    u = IxIy_sum * IyIt_sum - Iy2_sum * IxIt_sum
+    v = IxIt_sum * IxIy_sum - Ix2_sum * IyIt_sum
+    u = u / det
+    v = v / det
 
+    # print(u.max())
     return u, v
 
 
@@ -76,9 +78,6 @@ def main():
     image1 = cv2.GaussianBlur(image1_orig, mask, 0)
     image2 = cv2.GaussianBlur(image2_orig, mask, 0)
 
-    # image1 = image1_orig
-    # image2 = image2_orig
-
     u, v = lucas_kanade(image1, image2)
 
     # Plot 
@@ -86,5 +85,41 @@ def main():
     plt.show()
 
 
+def draw_arrows(image, u, v):
+    mesh_padding = 7
+    for i in range(0, u.shape[0], mesh_padding):
+            for j in range(0, u.shape[1], mesh_padding):
+                    cv2.arrowedLine(image, (i, j), (i + int(u[i,j]), j + int(v[i,j])), (0,0,255))
+
+    return image
+
+def feed_from_cam():
+    mask = (51, 51)
+
+    cap = cv2.VideoCapture(0)
+    ret, prev_frame = cap.read()
+    prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    prev_frame = cv2.GaussianBlur(prev_frame, mask, 0)
+
+    while (True):
+        ret, frame = cap.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.GaussianBlur(frame_gray, mask, 0)
+
+        u, v = lucas_kanade(prev_frame, frame_gray)
+
+        frame_w_flow = draw_arrows(frame_gray, u, v)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame_w_flow)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        prev_frame = frame_gray.copy()
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
 if __name__ == "__main__":
-    main()
+    feed_from_cam()
